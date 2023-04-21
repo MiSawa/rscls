a21a0b386dec34650c97ddede2c9374744d3dd60
mi_sawa <mi.sawa.1216+git@gmail.com>
2023-04-21 21:50:52 +0900

    Remove dependency to rust-analyzer.project-model

--- src/script.rs
+++ src/script.rs
@@ -9,50 +9,41 @@ use std::{
 };
 
 use eyre::{ensure, eyre, Result, WrapErr as _};
+use once_cell::sync::Lazy;
 use path_absolutize::Absolutize as _;
-use project_model::Sysroot;
-use serde::Serialize;
 use serde_json::{json, Value};
 
 use crate::event::EventSender;
 
-#[derive(PartialEq, Eq)]
-enum CachedProject {
-    InMemory { project: Value },
-    InFile { manifest: PathBuf },
-}
-impl Serialize for CachedProject {
-    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        match self {
-            CachedProject::InMemory { project } => project.serialize(serializer),
-            CachedProject::InFile { manifest } => manifest.serialize(serializer),
-        }
-    }
-}
-
 struct Script {
     source: PathBuf,
     rust_script: Arc<PathBuf>,
-    project: RwLock<Arc<CachedProject>>,
+    fallback_project: Value,
+    project: RwLock<Arc<Option<PathBuf>>>,
     need_refresh: AtomicBool,
     refresh_lock: Mutex<()>,
 }
 impl Script {
-    fn new(source: PathBuf, project: Value, rust_script: Arc<PathBuf>) -> Self {
+    fn new(source: PathBuf, rust_script: Arc<PathBuf>) -> Self {
+        let fallback_project = create_default_project(&source);
         Self {
             source,
             rust_script,
-            project: RwLock::new(Arc::new(CachedProject::InMemory { project })),
+            fallback_project,
+            project: RwLock::new(Arc::new(None)),
             need_refresh: AtomicBool::new(false),
             refresh_lock: Mutex::new(()),
         }
     }
 
-    fn project(&self) -> Arc<CachedProject> {
-        self.project.read().unwrap().clone()
+    fn project(&self) -> Value {
+        let tmp = self.project.read().unwrap().clone();
+        if let Some(manifest) = tmp.as_ref() {
+            if manifest.exists() {
+                return serde_json::to_value(manifest).unwrap();
+            }
+        }
+        self.fallback_project.clone()
     }
 
     fn queue_refresh(self: &Arc<Self>, refreshed: impl FnOnce() + Send + 'static) {
@@ -68,9 +59,7 @@ impl Script {
         }
         let ret = || -> Result<()> {
             let project_dir = package_dir(self.rust_script.as_ref(), self.source.as_path())?;
-            let new_project = CachedProject::InFile {
-                manifest: project_dir.join("Cargo.toml"),
-            };
+            let new_project = Some(project_dir.join("Cargo.toml"));
             let mut project_write = self.project.write().unwrap();
             if project_write.as_ref() != &new_project {
                 *project_write = new_project.into();
@@ -91,30 +80,20 @@ pub struct Scripts {
     event_sender: EventSender,
     rust_script: Arc<PathBuf>,
     scripts: BTreeMap<lsp_types::Url, Arc<Script>>,
-    fallback_sysroot: Sysroot,
 }
 impl Scripts {
     pub fn new(event_sender: EventSender, rust_script: PathBuf) -> Result<Self> {
-        let fallback_sysroot =
-            Sysroot::discover(Path::new("/").try_into().unwrap(), &Default::default())
-                .map_err(|e| eyre!("unable to locate fallback sysroot: {e:?}"))?;
         Ok(Self {
             event_sender,
             rust_script: rust_script.into(),
             scripts: BTreeMap::new(),
-            fallback_sysroot,
         })
     }
 
     pub fn register(&mut self, uri: lsp_types::Url) {
         if let Ok(file) = uri.to_file_path() {
             if let std::collections::btree_map::Entry::Vacant(entry) = self.scripts.entry(uri) {
-                let project = create_default_project(&self.fallback_sysroot, &file);
-                let script = entry.insert(Arc::new(Script::new(
-                    file,
-                    project,
-                    self.rust_script.clone(),
-                )));
+                let script = entry.insert(Arc::new(Script::new(file, self.rust_script.clone())));
                 let sender = self.event_sender.clone();
                 sender.mark_need_reload();
                 script.queue_refresh(move || {
@@ -142,20 +121,49 @@ impl Scripts {
     pub fn projects(&self) -> Vec<Value> {
         self.scripts
             .values()
-            .map(|script| serde_json::to_value(script.project().as_ref()).unwrap())
+            .map(|script| script.project())
             .collect()
     }
 }
 
+fn create_default_project(source: &PathBuf) -> Value {
+    static SYSROOT: Lazy<Result<PathBuf>> = Lazy::new(default_sysroot);
+    let mut value = json!({
+        "crates": [{
+            "root_module": source,
+            "edition": "2021",
+            "deps": [],
+            "is_proc_macro": false,
+        }]
+    });
+    if let Ok(Ok(sysroot)) = SYSROOT.as_ref().map(serde_json::to_value) {
+        value
+            .as_object_mut()
+            .unwrap()
+            .insert("sysroot".to_owned(), sysroot);
+    }
+    value
+}
+
 fn package_dir(rust_script: impl AsRef<Path>, script: impl AsRef<Path>) -> Result<PathBuf> {
-    let output = Command::new(rust_script.as_ref())
-        .arg("--package")
-        .arg(script.as_ref())
+    let mut cmd = Command::new(rust_script.as_ref());
+    cmd.arg("--package").arg(script.as_ref());
+    run_and_parse_output_as_path(cmd)
+}
+
+fn default_sysroot() -> Result<PathBuf> {
+    let mut cmd = Command::new("rustc");
+    cmd.args(["--print", "sysroot"]).current_dir("/");
+    run_and_parse_output_as_path(cmd)
+}
+
+fn run_and_parse_output_as_path(mut command: Command) -> Result<PathBuf> {
+    let output = command
         .output()
-        .wrap_err("failed to spawn rust-script")?;
+        .wrap_err_with(|| eyre!("failed to run `{command:?}`"))?;
     ensure!(
         output.status.success(),
-        "rust-script process terminated with a nonzero exit code {} with stderr {}",
+        "`{command:?}` terminated with a nonzero exit status {} with stderr {}",
         output.status,
         String::from_utf8_lossy(&output.stderr)
     );
@@ -163,19 +171,4 @@ fn package_dir(rust_script: impl AsRef<Path>, script: impl AsRef<Path>) -> Resul
     let path = PathBuf::from(output.trim_end());
     let path = path.absolutize().wrap_err("got an invalid abs path")?;
     Ok(path.to_path_buf())
-}
-
-fn create_default_project(sysroot: &Sysroot, source: &PathBuf) -> Value {
-    let root: PathBuf = sysroot.root().to_owned().into();
-    let src_root: PathBuf = sysroot.src_root().to_owned().into();
-    json!({
-        "sysroot": root,
-        "src_root": src_root,
-        "crates": [{
-            "root_module": source,
-            "edition": "2021",
-            "deps": [],
-            "is_proc_macro": false,
-        }]
-    })
 }
