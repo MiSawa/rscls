9336a6de1d3dd8ea41dda79840ae5d680de2b2a6
mi_sawa <mi.sawa.1216+git@gmail.com>
2026-02-14 15:16:05 +0900

    Add flake

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = "A language server for rust-script";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    systems.url = "github:nix-systems/default";
+    fenix = {
+      url = "github:nix-community/fenix/monthly";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [ inputs.devshell.flakeModule ];
+      perSystem =
+        {
+          system,
+          pkgs,
+          ...
+        }:
+        let
+          toolchain = pkgs.fenix.complete.toolchain;
+          minimalToolchain = pkgs.fenix.complete.withComponents [
+            "rustc"
+            "cargo"
+          ];
+        in
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.fenix.overlays.default
+            ];
+          };
+          packages.default =
+            (pkgs.makeRustPlatform {
+              cargo = minimalToolchain;
+              rustc = minimalToolchain;
+            }).buildRustPackage
+              {
+                pname = "rscls";
+                version = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.version;
+                src = ./.;
+                cargoLock.lockFile = ./Cargo.lock;
+              };
+          devshells.default = {
+            commands = [
+              {
+                name = "release";
+                category = "development";
+                help = "release to crates.io";
+                command = ''nix shell 'nixpkgs#cargo-release' --command 'cargo' 'release' "$@"'';
+              }
+            ];
+            packages = [
+              pkgs.clang
+              toolchain
+            ];
+          };
+        };
+    };
+}
